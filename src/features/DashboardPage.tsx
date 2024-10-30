6f1b82a556fcdb7a07b6f27176a1b5803e29aaf4
smmuja <smmuja27@gmail.com>
2024-10-30 08:57:44 +0700

    feat: add chart and datepicker functions

--- src/features/DashboardPage.tsx
+++ src/features/DashboardPage.tsx
@@ -4,16 +4,30 @@ import {
   DatePickerComponent,
   ChartComponent,
 } from "./DashboardPage/components";
+import { useState } from "react";
+import { DatePickerData } from "./DashboardPage/data";
 
 export function DashboardPage() {
+  const [startDate, setStartDate] = useState<Date | undefined>(
+    DatePickerData[0].month
+  ); // default to first month
+  const [endDate, setEndDate] = useState<Date | undefined>(
+    DatePickerData[DatePickerData.length - 1].month
+  ); // default to last month
+
   return (
     <>
       <DashboardLayout>
         <div>
-          <DatePickerComponent />
+          <DatePickerComponent
+            startDate={startDate}
+            endDate={endDate}
+            onStartDateChange={setStartDate}
+            onEndDateChange={setEndDate}
+          />
         </div>
         <div>
-          <ChartComponent />
+          <ChartComponent startDate={startDate} endDate={endDate} />
         </div>
       </DashboardLayout>
     </>
