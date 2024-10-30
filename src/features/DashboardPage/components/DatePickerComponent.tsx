68c9dae4c727b2dc7c24a2ec52099325fd14d22f
smmuja <smmuja27@gmail.com>
2024-10-30 08:57:44 +0700

    feat: add chart and datepicker functions

--- src/features/DashboardPage/components/DatePickerComponent.tsx
+++ src/features/DashboardPage/components/DatePickerComponent.tsx
@@ -1,20 +1,54 @@
 import { DatePickerData as data } from "@/features/DashboardPage/data";
 
-export function DatePickerComponent() {
+type DatePickerProps = {
+  startDate: Date | undefined;
+  endDate: Date | undefined;
+  onStartDateChange: (date: Date) => void;
+  onEndDateChange: (date: Date) => void;
+};
+
+export function DatePickerComponent(props: DatePickerProps) {
+  const { startDate, endDate, onStartDateChange, onEndDateChange } = props;
+
+  const formatDateToMonthYear = (date?: Date) =>
+    date
+      ? date.toLocaleString("default", { month: "long", year: "numeric" })
+      : "";
+
   return (
     <>
-      <select name="" id="" className="p-3 m-3 rounded-lg">
+      <select
+        name=""
+        id=""
+        value={formatDateToMonthYear(startDate)}
+        onChange={(e) => onStartDateChange(new Date(e.target.value))}
+        className="p-3 m-3 rounded-lg"
+      >
         {data.map((data) => (
-          <option key={data.month} value={data.month}>
-            {data.month}
+          <option
+            key={formatDateToMonthYear(data.month)}
+            value={formatDateToMonthYear(data.month)}
+            disabled={endDate && data.month > endDate}
+          >
+            {formatDateToMonthYear(data.month)}
           </option>
         ))}
       </select>
       <span>~</span>
-      <select name="" id="" className="p-3 m-3 rounded-lg">
+      <select
+        name=""
+        id=""
+        value={formatDateToMonthYear(endDate)}
+        onChange={(e) => onEndDateChange(new Date(e.target.value))}
+        className="p-3 m-3 rounded-lg"
+      >
         {data.map((data) => (
-          <option key={data.month} value={data.month}>
-            {data.month}
+          <option
+            key={formatDateToMonthYear(data.month)}
+            value={formatDateToMonthYear(data.month)}
+            disabled={startDate && data.month < startDate}
+          >
+            {formatDateToMonthYear(data.month)}
           </option>
         ))}
       </select>
