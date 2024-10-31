1d8dd44a9f0d60a8921cd898b424f5366c53951b
smmuja <smmuja27@gmail.com>
2024-10-31 15:32:48 +0700

    feat: total timespent chart and dashboard

---
+++ src/features/DashboardPage/components/TimespentChartComponent.tsx
@@ -0,0 +1,35 @@
+//
+import { Line } from "react-chartjs-2";
+import { useChart } from "@/hooks";
+import { DatePickerData } from "../data";
+
+type ChartComponentProps = {
+  startDate?: Date;
+  endDate?: Date;
+  totalView: number[];
+};
+
+export function TimespentChartComponent({
+  startDate,
+  endDate,
+}: ChartComponentProps) {
+  const timeSpent = DatePickerData.map((data) => data.timespent);
+
+  const label = "Timespent chart";
+  // const datasets = totalView;
+
+  const { data, options } = useChart({
+    startDate,
+    endDate,
+    label,
+    datasets: timeSpent,
+  });
+
+  return (
+    <>
+      <div style={{ width: "1000px", margin: "0 auto" }}>
+        <Line data={data} options={options} />
+      </div>
+    </>
+  );
+}
