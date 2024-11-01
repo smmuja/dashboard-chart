c37d6befc829a11ebc826a297010dbdb36f51bfa
smmuja <smmuja27@gmail.com>
2024-11-01 10:37:52 +0700

    feat: utils date formatter date day

--- src/utils/dateFormatter.ts
+++ src/utils/dateFormatter.ts
@@ -7,3 +7,20 @@ export function formatMonthYear(date?: Date): string | undefined {
 export function formatMonthShort(date?: Date): string | undefined {
   return date ? date.toLocaleString("default", { month: "short" }) : undefined;
 }
+
+export function formatDateDay(date: Date): string {
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  const formattedDateParts = date
+    .toLocaleDateString("en-GB", options)
+    .split(" ");
+  const weekday = formattedDateParts[0];
+  const day = formattedDateParts[1];
+  const month = formattedDateParts[2];
+
+  return `${weekday}, ${day} ${month}`;
+}
