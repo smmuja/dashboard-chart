afb25243e4d8efd5ef77a40e3c8fcc2bbfdcb662
smmuja <smmuja27@gmail.com>
2024-11-01 13:18:52 +0700

    style: adjust datepicker component style

--- src/features/DashboardPage/components/DatePickerComponent.tsx
+++ src/features/DashboardPage/components/DatePickerComponent.tsx
@@ -1,5 +1,6 @@
 import { MetricsData as data } from "@/data";
 import { formatMonthYear } from "@/utils";
+import { GoCalendar } from "react-icons/go";
 
 type DatePickerProps = {
   startDate: Date | undefined;
@@ -13,41 +14,49 @@ export function DatePickerComponent(props: DatePickerProps) {
 
   return (
     <>
-      <select
-        name=""
-        id=""
-        value={formatMonthYear(startDate)}
-        onChange={(e) => onStartDateChange(new Date(e.target.value))}
-        className="p-3 m-3 rounded-lg"
-      >
-        {data.map((data) => (
-          <option
-            key={formatMonthYear(data.month)}
-            value={formatMonthYear(data.month)}
-            disabled={endDate && data.month > endDate}
-          >
-            {formatMonthYear(data.month)}
-          </option>
-        ))}
-      </select>
-      <span>~</span>
-      <select
-        name=""
-        id=""
-        value={formatMonthYear(endDate)}
-        onChange={(e) => onEndDateChange(new Date(e.target.value))}
-        className="p-3 m-3 rounded-lg"
-      >
-        {data.map((data) => (
-          <option
-            key={formatMonthYear(data.month)}
-            value={formatMonthYear(data.month)}
-            disabled={startDate && data.month < startDate}
-          >
-            {formatMonthYear(data.month)}
-          </option>
-        ))}
-      </select>
+      <div className="flex flex-row justify-end">
+        <div className="flex flex-row items-center justify-end border rounded-xl border-gray-400 px-3 m-3 w-fit">
+          <p>Range: </p>
+          <div className="flex flex-row items-center px-3">
+            <GoCalendar />
+            <select
+              name=""
+              id=""
+              value={formatMonthYear(startDate)}
+              onChange={(e) => onStartDateChange(new Date(e.target.value))}
+              className="p-3 m-3 rounded-lg bg-white "
+            >
+              {data.map((data) => (
+                <option
+                  key={formatMonthYear(data.month)}
+                  value={formatMonthYear(data.month)}
+                  disabled={endDate && data.month > endDate}
+                >
+                  {formatMonthYear(data.month)}
+                </option>
+              ))}
+            </select>
+            <span>~</span>
+            <select
+              name=""
+              id=""
+              value={formatMonthYear(endDate)}
+              onChange={(e) => onEndDateChange(new Date(e.target.value))}
+              className="p-3 m-3 rounded-lg bg-white "
+            >
+              {data.map((data) => (
+                <option
+                  key={formatMonthYear(data.month)}
+                  value={formatMonthYear(data.month)}
+                  disabled={startDate && data.month < startDate}
+                >
+                  {formatMonthYear(data.month)}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
